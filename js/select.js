e09fbaf0616f01153e52adb97de9208083ffef5a
e <you@example.com>
2018-09-29 15:51:36 +0800

    yhf

--- js/select.js
+++ js/select.js
@@ -1,6 +1,7 @@
 function select_effect(tui,select) {
     tui.click(function () {
         let index=$(this).index()-1;
+        texta=$(this).children().children().children().next().text();
         select.css("opacity","0");
         select.eq(index).css("opacity","1");
     })
